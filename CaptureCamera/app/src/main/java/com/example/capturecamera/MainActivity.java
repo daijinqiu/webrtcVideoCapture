8c30fdf61de298a982b854f693584940b977c0f5
daijinqiu <daijinqiu2008@163.com>
2020-05-02 20:11:02 +0800

    加camera1

--- CaptureCamera/app/src/main/java/com/example/capturecamera/MainActivity.java
+++ CaptureCamera/app/src/main/java/com/example/capturecamera/MainActivity.java
@@ -1,18 +1,104 @@
 package com.example.capturecamera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import com.example.capturecamera.opengl.*;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.capturecamera.cameras.Camera1Enumerator;
+import com.example.capturecamera.cameras.CameraEnumerator;
+import com.example.capturecamera.cameras.RendererCommon.ScalingType;
+import com.example.capturecamera.cameras.SurfaceTextureHelper;
+import com.example.capturecamera.cameras.SurfaceViewRenderer;
+import com.example.capturecamera.cameras.VideoCapturer;
+import com.example.capturecamera.opengl.EglBase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "MainActivity";
     final EglBase eglBase = EglBase.create();
+    private SurfaceViewRenderer renderer;
+    private VideoCapturer videoCapturer;
+    private SurfaceTextureHelper surfaceTextureHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        request();
+        renderer = findViewById(R.id.fullscreen_video_view);
+        renderer.init(eglBase.getEglBaseContext(), null);
+        renderer.setScalingType(ScalingType.SCALE_ASPECT_FILL);
+        renderer.setZOrderMediaOverlay(true);
+        renderer.setEnableHardwareScaler(true);
+
+
+
+
     }
+
+    private  VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // First, try to find front facing camera
+        Log.d(TAG, "Looking for front facing cameras.");
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                Log.d(TAG, "Creating front facing camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // Front facing camera not found, try something else
+        Log.d(TAG, "Looking for other cameras.");
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                Log.d(TAG, "Creating other camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private void request(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, " grad");
+            ActivityCompat.requestPermissions(this,
+                    REQUESTED_PERMISSIONS, 1);
+            videoCapturer = createCameraCapturer(new Camera1Enumerator(true));
+
+            surfaceTextureHelper =
+                    SurfaceTextureHelper.create("CaptureThread", eglBase.getEglBaseContext());
+            videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), null);
+            videoCapturer.startCapture(640, 480, 20);
+        }else {
+            Log.d(TAG, " not grad");
+            videoCapturer = createCameraCapturer(new Camera1Enumerator(true));
+
+            surfaceTextureHelper =
+                    SurfaceTextureHelper.create("CaptureThread", eglBase.getEglBaseContext());
+            videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(), null);
+            videoCapturer.startCapture(640, 480, 20);
+        }
+
+    }
+
 }
--- CaptureCamera/app/src/main/java/com/example/capturecamera/camera.java
+++
@@ -1,4 +0,0 @@
-package com.example.capturecamera;
-
-public class camera {
-}
